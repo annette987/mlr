591873463d4d91c75bd8fc93eef63ac83e51382a
Annette Spooner <a.spooner@unsw.edu.au>
2020-04-18 20:51:39 +1000

    Remove debugging

--- R/FilterEnsemble.R
+++ R/FilterEnsemble.R
@@ -378,6 +378,5 @@ mergeFilters = function(simple_filters, ensemble_filters) {
   # merge ensemble and base filters
   simple_filters$rank = NULL
   all.filters = rbind(simple_filters, ensemble_filters)
-	print("rbind complete")
   return(all.filters)
 }
